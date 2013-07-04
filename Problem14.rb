b9cf9149e26c02389b1ff3aa51ae7bc9f92e6bfd
Melissa Holmes <meltar@gmail.com>
2013-07-04 01:11:11 -0400

    Add Problem 14

---
+++ Problem14.rb
@@ -0,0 +1,46 @@
+# The following iterative sequence is defined for the set of positive integers:
+# 
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+# 
+# Using the rule above and starting with 13, we generate the following sequence:
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# 
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. 
+# Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+# 
+# Which starting number, under one million, produces the longest chain?
+# 
+# NOTE: Once the chain starts the terms are allowed to go above one million.
+
+def generate_chain(start)
+  num = start
+  chain = [num]
+  while num > 1
+    if num.even?
+      num = num/2
+    else
+      num = 3 * num + 1
+    end
+    chain << num
+  end
+  chain.count
+end
+
+def find_longest_chain
+  max_chain_start = 0
+  max_chain_length = 0
+  start = 1000000
+  while start > 1 do
+    length = generate_chain(start)
+    if length > max_chain_length
+      max_chain_length = length
+      max_chain_start = start
+    end
+    start -= 1
+  end
+  max_chain_start
+end
+
+#puts generate_chain(13)
+puts find_longest_chain
