8dda0542dcab57bcc79052d30447248312a91274
Melissa Holmes <meltar@gmail.com>
2013-07-04 00:21:10 -0400

    Small improvement to Problem 13

--- Problem13.rb
+++ Problem13.rb
@@ -1,4 +1,5 @@
 # Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+#
 # 37107287533902102798797998220837590246510135740250
 # 46376937677490009712648124896970078050417018260538
 # 74324986199524741059474233309513058123726617309629
@@ -105,7 +106,7 @@ def read_list(filename)
 end
 
 def first_digits_of_sum(data, digits = 10)
-  data.inject(:+).to_s.slice(0, digits)
+  data.reduce(:+).to_s.slice(0, digits)
 end
 
 data = read_list('Problem13_source.txt')
