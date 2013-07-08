c1abc913d647361b35927c28f54626433c69f8eb
Melissa Holmes <meltar@gmail.com>
2013-07-08 09:54:11 -0400

    Problem 17 complete

--- Problem17.rb
+++ Problem17.rb
@@ -17,7 +17,7 @@ stored_words = { '1'=> 'one', '2'=> 'two', '3'=> 'three', '4'=> 'four', '5'=> 'f
 def word_form_of_number(words, value, accumulated_string)
   word_form = accumulated_string
   if value < 20 || value == 1000
-    word_form << words.fetch(value.to_s)
+    word_form << "#{words.fetch(value.to_s)} "
   elsif value < 100
     temp_val = "#{value.to_s.each_char.first}0"
     word_form << words.fetch(temp_val)
@@ -26,23 +26,24 @@ def word_form_of_number(words, value, accumulated_string)
       word_form = word_form_of_number(words, value - temp_val.to_i, word_form)
     end
   else
-    # FINISH THIS!
+    temp_val = "#{value.to_s.each_char.first}"
+    word_form << words.fetch(temp_val)
+    word_form << " hundred"
+    if value - temp_val.to_i * 100 > 0
+      word_form << " and "
+      word_form = word_form_of_number(words, value - temp_val.to_i * 100, word_form)
+    end 
   end
   word_form
 end
 
-puts word_form_of_number(stored_words, 15, '')
-puts word_form_of_number(stored_words, 35, '')
-puts word_form_of_number(stored_words, 11, '')
-puts word_form_of_number(stored_words, 61, '')
-puts word_form_of_number(stored_words, 99, '')
+def letter_count(string)
+  count = 0
+  string.each_char { |c| count += 1 if c >= 'a' && c <= 'z' }
+  count
+end
 
-# 
-# (1..10).each do |number| 
-#   if number >= 20
-#     word_form = numbers.fetch(number.to_s, '')
-#   elsif number > 100
-#     word_form = 
-#   end
-#   puts word_form
-# end
+
+range_in_words = ''
+(1..1000).each { |number| range_in_words << word_form_of_number(stored_words, number, '') }
+puts letter_count(range_in_words)
