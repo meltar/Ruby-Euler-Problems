a351feb37b1ea849fa0fdaa883eb78e32f60f602
Melissa Holmes <meltar@gmail.com>
2014-01-24 22:46:13 -0500

    Complete problem 4

---
+++ 2014/problem4.rb
@@ -0,0 +1,40 @@
+# Largest palindrome product
+# Problem 4
+#
+# A palendromic number reads the same both ways.  The largest palindrome made
+# from the product of two 2-digit numbers is 9009 = 91 x 99
+#
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+def largest_palindrome digits
+	largest_found = 1
+
+	min = range_min digits
+	max = range_max digits
+
+	(min..max).each do |x|
+		(x..max).each do |y|
+			product = x * y
+			if product.to_s == product.to_s.reverse && product > largest_found
+				largest_found = product
+			end
+		end
+	end
+
+	largest_found
+end
+
+def range_max digits
+	max = 1
+	digits.times { max = max * 10 }
+	max
+end
+
+def range_min digits
+	min = 1
+	(digits - 1).times { min = min * 10 }
+	min = min - 1
+end
+
+#puts largest_palindrome 2 
+puts largest_palindrome 3
